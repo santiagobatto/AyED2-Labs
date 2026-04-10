93e5bee9b73f4887b0a92d87708ea1ec9849a394
santiagobatto <santiagobatto007@gmail.com>
2026-04-10 18:50:10 -0300

    punto 5 en curso

--- Lab01_Ordenación/lab01/ej5/fixstring.c
+++ Lab01_Ordenación/lab01/ej5/fixstring.c
@@ -5,15 +5,29 @@
 
 
 unsigned int fstring_length(fixstring s) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
-}
+     unsigned int length = 0;
+    
+    for (unsigned int i = 0; s[i] != '\0'; i++) {
+        length++;    
+    }
+    return length;}
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
-}
+    unsigned int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '0' && s1[i] == s2[i]) {
+        i++;
+    }
+    return (s1[i] == s2[i]);}
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    unsigned int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]) { //Busco el primer caracter donde sean diferentes
+        i++;
+    }
+
+    return (s1[i] <= s2[i]); //En la pos donde son diferentes, evaluo si es menor
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
