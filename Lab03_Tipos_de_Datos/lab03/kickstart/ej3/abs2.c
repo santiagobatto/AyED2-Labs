2ea7e38e0b0773d2e795f8b014cfdbcb51fb30ed
santiagobatto <santiagobatto007@gmail.com>
2026-04-24 16:18:46 -0300

    lab3

---
+++ Lab03_Tipos_de_Datos/lab03/kickstart/ej3/abs2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void absolute(int x, int *y) {
+    //
+    // Completar aquí
+    //
+}
+
+int main(void) {
+    int a=0, res=0;  // No modificar esta declaración
+    // --- No se deben declarar variables nuevas ---
+
+    //
+    // Completar aquí
+    //
+    assert(res >= 0 && (res == a || res == -a));
+    return EXIT_SUCCESS;
+}
+
---
+++ Lab03_Tipos_de_Datos/lab03/kickstart/ej3/swap.c
@@ -0,0 +1,3 @@
+//
+// Completar aquí
+//
